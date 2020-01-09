d99f787fedba0de91b4be803dd0c3da033679e46
frankwolffnl <frankwolff.nl@gmail.com>
2020-01-09 09:23:54 +0100

    Added dbconfig

--- webapp/includes/dbConfig.php
+++ webapp/includes/dbConfig.php
@@ -2,8 +2,4 @@
 $SERVER_NAME = 'localhost';
 $DB_USERNAME = 'root';
 $DB_PASSWORD = 'root';
-<<<<<<< HEAD
 $DB_NAME = 'weatherstation';
-=======
-$DB_NAME = 'weatherstation-dummy';
->>>>>>> date-xaxis
